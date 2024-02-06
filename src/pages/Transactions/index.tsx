d43de0ac39458a4cfa047c3c00eb85af38d02934
Kathleen Santos <kathleen.crissantos15@gmail.com>
2024-02-05 21:51:29 -0300

    feat: get infos transactions from local back-end improvised

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./Components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
+interface Transactions {
+    id: number,
+    title: string,
+    type: "income" | 'outcome',
+    category: string,
+    price: number,
+    createdAt: string
+}
+
 export function Transactions () {
+
+    const [transactions, setTransactions] = useState<Transactions[]>([]);
+
+    async function historyTransactions () {
+        const response = await fetch('http://localhost:3000/transactions')
+        const data = await response.json()
+        setTransactions(data)
+    }
+
+    useEffect(() => {
+        try {
+            historyTransactions()
+        } catch (error) {
+            console.error('Erro na requisição GET:', error);
+        }
+    }, [])
+
     return (
         <div>
             <Header />
@@ -14,17 +42,19 @@ export function Transactions () {
             <SearchForm />
             <TransactionsTable>
                 <tbody>
-                    <tr>
-                        <td width='50%'> Desenvolvimento de site</td>
-                        <td>  
-                            <PriceHighlight variant="income">
-                                R$ 12.000
-                            </PriceHighlight>
-                        </td>
-                        <td>Venda</td>
-                        <td>13/04/2022</td>
-                    </tr>
-                    <tr>
+                    {transactions.map((transaction) => (
+                        <tr>
+                            <td width='50%'>{transaction.title}</td>
+                            <td>  
+                                <PriceHighlight variant={transaction.type}>
+                                    R${transaction.price}
+                                </PriceHighlight>
+                            </td>
+                            <td>{transaction.category}</td>
+                            <td>{transaction.createdAt}</td>
+                        </tr>
+                    ))}
+                    {/* <tr>
                         <td width='50%'> Desenvolvimento de site</td>
                         <td>
                             <PriceHighlight variant="outcome">
@@ -33,7 +63,7 @@ export function Transactions () {
                         </td>
                         <td>Venda</td>
                         <td>13/04/2022</td>
-                    </tr>
+                    </tr> */}
                 </tbody>
             </TransactionsTable>
         </TransactionsContainer>
--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const TransactionsContainer = styled.div`
     width: 100%;
     max-width: 1120px;
-    margin: 4rem auto 0;
+    margin: 4rem auto 4rem;
 
 `
 
